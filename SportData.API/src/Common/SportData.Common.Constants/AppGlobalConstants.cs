52b598b3efa48b5ad24ffd0861caa3752a34067b
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-01-11 13:50:47 +0200

    Library was initialized.

---
+++ SportData.API/src/Common/SportData.Common.Constants/AppGlobalConstants.cs
@@ -0,0 +1,21 @@
+﻿namespace SportData.Common.Constants;
+
+public static class AppGlobalConstants
+{
+    public const string LOGGING = "Logging";
+    public const string LOG4NET_CORE = "Log4NetCore";
+
+    public const string APP_SETTINGS_FILE = "appsettings.json";
+
+    public const string CRAWLER_STORAGE_CONNECTION_STRING = "CrawlerStorageConnectionString";
+    public const string OLYMPIC_GAMES_CONNECTION_STRING = "OlympicGamesConnectionString";
+    public const string SPORT_DATA_CONNECTION_STRING = "SportDataConnectionString";
+
+    public const string AUTOMAPPER_MODELS_ASSEMBLY = "SportData.Data.Models";
+
+    public const string JWT_SECRET = "JWT:Secret";
+    public const string JWT_ISSUER = "JWT:Issuer";
+    public const string JWT_AUDIENCE = "JWT:Audience";
+    public const string JWT_SUBJECT = "JWT:Subject";
+
+}
