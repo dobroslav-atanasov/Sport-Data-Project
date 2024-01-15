711e56cb1f9519364540df8b9aee778766fcc1ac
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-01-15 15:16:08 +0200

    Custom filter, middlewares and logfile added.

---
+++ SportData.API/src/Web/SportData.WebAPI.Infrastructure/Middlewares/CustomeMiddleware.cs
@@ -0,0 +1,24 @@
+﻿namespace SportData.WebAPI.Infrastructure.Middlewares;
+
+using Microsoft.AspNetCore.Http;
+
+public class CustomeMiddleware
+{
+    private readonly RequestDelegate next;
+
+    public CustomeMiddleware(RequestDelegate next)
+    {
+        this.next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        await context.Response.WriteAsync("1");
+        if (DateTime.Now.Second % 2 == 0)
+        {
+            await this.next(context);
+        }
+
+        await context.Response.WriteAsync("2");
+    }
+}
---
+++ SportData.API/src/Web/SportData.WebAPI.Infrastructure/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,46 @@
+﻿namespace SportData.WebAPI.Infrastructure.Middlewares;
+
+using System.Net;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+using SportData.Data.Models.Errors;
+
+public class ExceptionMiddleware
+{
+    private readonly RequestDelegate next;
+    private readonly ILogger logger;
+
+    public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+    {
+        this.next = next;
+        this.logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await this.next(context);
+        }
+        catch (Exception ex)
+        {
+
+            this.logger.LogError($"{ex}");
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    private async Task HandleExceptionAsync(HttpContext context, Exception ex)
+    {
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+        await context.Response.WriteAsync(new ErrorDetails()
+        {
+            StatusCode = context.Response.StatusCode,
+            Message = "Internal Server Error"
+        }.ToString());
+    }
+}
