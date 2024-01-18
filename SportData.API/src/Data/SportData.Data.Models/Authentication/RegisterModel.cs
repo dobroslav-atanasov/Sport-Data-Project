c41a1f75fa29f46702e3f29a6d88a6d43d2db601
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-01-18 09:18:00 +0200

    Added new property "Role".

--- SportData.API/src/Data/SportData.Data.Models/Authentication/RegisterModel.cs
+++ SportData.API/src/Data/SportData.Data.Models/Authentication/RegisterModel.cs
@@ -13,4 +13,6 @@ public class RegisterModel
     [EmailAddress]
     [Required(ErrorMessage = "Email is required!")]
     public string Email { get; set; }
+
+    public string Role { get; set; }
 }
