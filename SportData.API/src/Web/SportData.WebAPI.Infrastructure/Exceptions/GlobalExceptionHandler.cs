8de873c383dddd3f323aa2ad41d28b2e69047db9
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-01-29 15:49:27 +0200

    Global exception handler created.

---
+++ SportData.API/src/Web/SportData.WebAPI.Infrastructure/Exceptions/GlobalExceptionHandler.cs
@@ -0,0 +1,48 @@
+﻿namespace SportData.WebAPI.Infrastructure.Exceptions;
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+public class GlobalExceptionHandler : IExceptionHandler
+{
+    private readonly ILogger<GlobalExceptionHandler> logger;
+
+    public GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger)
+    {
+        this.logger = logger;
+    }
+
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        this.logger.LogError(exception, $"Exception occured");
+
+        var problemDetails = new ProblemDetails
+        {
+            Detail = exception.Message
+        };
+
+        switch (exception)
+        {
+            case BadHttpRequestException:
+                problemDetails.Status = StatusCodes.Status400BadRequest;
+                problemDetails.Title = "Bad Request";
+                break;
+            default:
+                problemDetails.Status = StatusCodes.Status500InternalServerError;
+                problemDetails.Title = "Internal Server Error";
+                break;
+        }
+
+        httpContext.Response.StatusCode = problemDetails.Status.Value;
+
+        await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+
+        return true;
+    }
+}
