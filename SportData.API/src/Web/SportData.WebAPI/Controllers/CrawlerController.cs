35c9509ec281423031317f09257313be03aad423
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-02-05 09:20:03 +0200

    Created controller.

---
+++ SportData.API/src/Web/SportData.WebAPI/Controllers/CrawlerController.cs
@@ -0,0 +1,31 @@
+﻿namespace SportData.WebAPI.Controllers;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+using SportData.Common.Constants;
+using SportData.Data.Models.Responses;
+
+[Authorize(Roles = $"{Roles.SUPERADMIN},{Roles.ADMIN}")]
+public class CrawlerController : BaseController
+{
+    [HttpGet]
+    [Route(RouteConstants.CRAWLER_START)]
+    public async Task<IActionResult> Start(int id)
+    {
+        switch (id)
+        {
+            case 1:
+
+                break;
+            default:
+                return this.BadRequest(new ResponseModel
+                {
+                    StatusCode = StatusCodes.Status400BadRequest,
+                    Message = "Invalid crawler!"
+                });
+        }
+
+        return this.Ok();
+    }
+}
