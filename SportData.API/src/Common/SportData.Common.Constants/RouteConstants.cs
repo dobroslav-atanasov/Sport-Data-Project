6df736c15320eff951304c8b2f12d3507193ce23
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-02-05 09:21:33 +0200

    Added constants.

--- SportData.API/src/Common/SportData.Common.Constants/RouteConstants.cs
+++ SportData.API/src/Common/SportData.Common.Constants/RouteConstants.cs
@@ -4,5 +4,13 @@ public static class RouteConstants
 {
     public const string API_DEFAULT_ROUTE = "/api/[controller]";
 
+    public const string AUTHENTICATE_REGISTER = "register";
+    public const string AUTHENTICATE_LOGIN = "login";
+    public const string AUTHENTICATE_REFRESH_TOKEN = "refresh-token";
+    public const string AUTHENTICATE_REVOKE = "revoke/{username}";
+    public const string AUTHENTICATE_REVOKE_ALL = "revoke-all";
+
     public const string CONVERTER_START = "Start";
+
+    public const string CRAWLER_START = "start/{id}";
 }
