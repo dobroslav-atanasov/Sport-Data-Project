7fe113a7f81752bf2b7d8713eb61b9a2279e230c
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-02-15 12:04:42 +0200

    Added new route.

--- sport-data/src/app/app-routing.module.ts
+++ sport-data/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './core/home/home.component';
+import { PageComponent } from './page/page.component';
+import { authGuard } from './shared/guards/auth.guard';
 
 const routes: Routes = [
+  {
+    path: 'page',
+    component: PageComponent,
+    canActivate: [authGuard]
+  },
   {
     path: '',
     pathMatch: 'full',
