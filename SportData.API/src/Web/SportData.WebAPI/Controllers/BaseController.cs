19ec3608eb6cb96c19ad29968e5b189ea4515df8
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-02-16 12:51:59 +0200

    Version added.

--- SportData.API/src/Web/SportData.WebAPI/Controllers/BaseController.cs
+++ SportData.API/src/Web/SportData.WebAPI/Controllers/BaseController.cs
@@ -1,10 +1,13 @@
 ﻿namespace SportData.WebAPI.Controllers;
 
+using Asp.Versioning;
+
 using Microsoft.AspNetCore.Mvc;
 
 using SportData.Common.Constants;
 
 [ApiController]
+[ApiVersion(1)]
 [Route(RouteConstants.API_DEFAULT_ROUTE)]
 public abstract class BaseController : ControllerBase
 {
