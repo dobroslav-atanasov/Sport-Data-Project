cbcbc43f8ca95848f95d2b42f8b27e596cd87d72
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-02-19 10:35:29 +0200

    Changed auth routing.

--- sport-data/src/app/auth/auth-routing.module.ts
+++ sport-data/src/app/auth/auth-routing.module.ts
@@ -2,21 +2,26 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
 
 const routes: Routes = [
-  {
-    path: 'auth',
-    children: [
-      {
-        path: 'register',
-        component: RegisterComponent
-      },
-      {
-        path: 'login',
-        component: LoginComponent
-      }
-    ]
-  }
+  // {
+  //   path: 'auth',
+  //   children: [
+  //     {
+  //       path: 'register',
+  //       component: RegisterComponent
+  //     },
+  //     {
+  //       path: 'login',
+  //       component: LoginComponent
+  //     },
+  //     {
+  //       path: 'logout',
+  //       component: LogoutComponent
+  //     }
+  //   ]
+  // }
 ];
 
 @NgModule({
