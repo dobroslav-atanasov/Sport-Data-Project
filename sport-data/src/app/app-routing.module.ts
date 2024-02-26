57291f8a12d25fd9a13c33d3ced8dd43bd1d61e8
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-02-26 15:34:50 +0200

    Lazy loading introduce.

--- sport-data/src/app/app-routing.module.ts
+++ sport-data/src/app/app-routing.module.ts
@@ -1,40 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './core/home/home.component';
-import { PageComponent } from './page/page.component';
-import { authGuard } from './shared/guards/auth.guard';
 import { NotFoundComponent } from './core/not-found/not-found.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { LoginComponent } from './auth/login/login.component';
-import { LogoutComponent } from './auth/logout/logout.component';
 
 const routes: Routes = [
   {
-    path: 'page',
-    component: PageComponent,
-    canActivate: [authGuard]
+    path: '',
+    pathMatch: 'full',
+    component: HomeComponent,
   },
   {
     path: 'auth',
-    children: [
-      {
-        path: 'register',
-        component: RegisterComponent
-      },
-      {
-        path: 'login',
-        component: LoginComponent
-      },
-      {
-        path: 'logout',
-        component: LogoutComponent
-      }
-    ]
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
   },
   {
-    path: '',
-    pathMatch: 'full',
-    component: HomeComponent
+    path: 'admin',
+    loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
   },
   {
     path: '**',
