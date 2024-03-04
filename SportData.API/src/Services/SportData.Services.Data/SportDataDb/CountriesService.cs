0ec9ca6bd5dc4b39546eeb23cafcb29363b25cd2
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-03-04 20:47:29 +0200

    Function GetAllAsync implemented.

--- SportData.API/src/Services/SportData.Services.Data/SportDataDb/CountriesService.cs
+++ SportData.API/src/Services/SportData.Services.Data/SportDataDb/CountriesService.cs
@@ -1,8 +1,12 @@
 ﻿namespace SportData.Services.Data.SportDataDb;
 
+using System.Collections.Generic;
+
 using SportData.Data.Models.Entities.SportData;
 using SportData.Data.Repositories;
+using SportData.Data.ViewModels.Countries;
 using SportData.Services.Data.SportDataDb.Interfaces;
+using SportData.Services.Mapper.Extensions;
 
 public class CountriesService : ICountriesService
 {
@@ -34,6 +38,15 @@ public class CountriesService : ICountriesService
         }
 
         return country;
+    }
+
+    public async Task<IEnumerable<CountryViewModel>> GetAllAsync()
+    {
+        var countries = this.repository
+            .AllAsNoTracking()
+            .To<CountryViewModel>();
+
+        return countries;
     }
 
     public async Task<Country> GetAsync(string code)
