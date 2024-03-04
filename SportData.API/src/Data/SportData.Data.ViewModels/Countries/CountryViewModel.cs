0fbbe9805d5dbfb11cea6db13c208bb86bf9c9d5
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-03-04 20:42:53 +0200

    View models added.

---
+++ SportData.API/src/Data/SportData.Data.ViewModels/Countries/CountryViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace SportData.Data.ViewModels.Countries;
+
+using SportData.Data.Models.Entities.SportData;
+using SportData.Services.Mapper.Interfaces;
+
+public class CountryViewModel : IMapFrom<Country>
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Code { get; set; }
+
+    public string TwoDigitsCode { get; set; }
+}
