c864548060e52f8761c5cefd2e6f894122175622
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-03-08 09:49:19 +0200

    The converter was modified.

--- SportData.API/src/Applications/SportData.Converters/OlympicGames/SportDisciplineConverter.cs
+++ SportData.API/src/Applications/SportData.Converters/OlympicGames/SportDisciplineConverter.cs
@@ -5,83 +5,108 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 
 using SportData.Data.Models.Entities.Crawlers;
+using SportData.Data.Models.Entities.OlympicGames;
+using SportData.Data.Repositories;
 using SportData.Services.Data.CrawlerStorageDb.Interfaces;
+using SportData.Services.Data.OlympicGamesDb.Interfaces;
 using SportData.Services.Interfaces;
 
 public class SportDisciplineConverter : BaseOlympediaConverter
 {
+    private readonly IDataCacheService dataCacheService;
+    private readonly OlympicGamesRepository<Sport> sportRepository;
+    private readonly OlympicGamesRepository<Discipline> disciplineRepository;
+
     public SportDisciplineConverter(ILogger<BaseConverter> logger, ICrawlersService crawlersService, ILogsService logsService, IGroupsService groupsService, IZipService zipService,
-        IRegExpService regExpService, INormalizeService normalizeService, IOlympediaService olympediaService)
+        IRegExpService regExpService, INormalizeService normalizeService, IOlympediaService olympediaService, IDataCacheService dataCacheService,
+        OlympicGamesRepository<Sport> sportRepository, OlympicGamesRepository<Discipline> disciplineRepository)
         : base(logger, crawlersService, logsService, groupsService, zipService, regExpService, normalizeService, olympediaService)
     {
+        this.dataCacheService = dataCacheService;
+        this.sportRepository = sportRepository;
+        this.disciplineRepository = disciplineRepository;
     }
 
     protected override async Task ProcessGroupAsync(Group group)
     {
-        //try
-        //{
-        //    var document = this.CreateHtmlDocument(group.Documents.Single());
-        //    var lines = document.DocumentNode.SelectNodes("//table[@class='table table-striped sortable']//tr");
+        try
+        {
+            var document = this.CreateHtmlDocument(group.Documents.Single());
+            var lines = document.DocumentNode.SelectNodes("//table[@class='table table-striped sortable']//tr");
 
-        //    foreach (var line in lines)
-        //    {
-        //        if (line.OuterHtml.Contains("glyphicon-ok"))
-        //        {
-        //            var elements = line.Elements("td").ToList();
-        //            var sportName = elements[2].InnerText.Trim();
-        //            if (sportName != "Air Sports" && sportName != "Mountaineering and Climbing" && sportName != "Art Competitions")
-        //            {
-        //                var type = elements[3].InnerText.Trim().ToEnum<OlympicGameType>();
-        //                var sportCode = this.RegExpService.MatchFirstGroup(elements[2].OuterHtml, @"/sport_groups/(.*?)""");
-        //                var sport = new Sport
-        //                {
-        //                    Name = sportName,
-        //                    Type = type,
-        //                    Code = sportCode
-        //                };
+            foreach (var line in lines)
+            {
+                if (line.OuterHtml.Contains("glyphicon-ok"))
+                {
+                    var elements = line.Elements("td").ToList();
+                    var name = elements[2].InnerText.Trim();
+                    if (name != "Air Sports" && name != "Mountaineering and Climbing" && name != "Art Competitions")
+                    {
+                        var olympicGameType = this.dataCacheService.OlympicGameTypes.FirstOrDefault(x => x.Name.Equals(elements[3].InnerText.Trim(), StringComparison.OrdinalIgnoreCase));
+                        var sportCode = this.RegExpService.MatchFirstGroup(elements[2].OuterHtml, @"/sport_groups/(.*?)""");
+                        var sport = new Sport
+                        {
+                            Name = name,
+                            OlympicGameTypeId = olympicGameType.Id,
+                            Code = sportCode
+                        };
 
-        //                //sport = await this.sportsService.AddOrUpdateAsync(sport);
+                        var dbSport = await this.sportRepository.GetAsync(x => x.Name == name);
+                        if (dbSport != null)
+                        {
+                            sport = dbSport;
+                        }
+                        else
+                        {
+                            await this.sportRepository.AddAsync(sport);
+                            await this.sportRepository.SaveChangesAsync();
+                        }
 
-        //                var disciplineName = elements[1].InnerText.Trim();
-        //                var disciplineAbbreviation = elements[0].InnerText.Trim();
-        //                var disciplines = new List<Discipline>();
-        //                if (sport.Name == "Wrestling")
-        //                {
-        //                    disciplines.Add(new Discipline
-        //                    {
-        //                        Name = "Wrestling Freestyle",
-        //                        Code = "WRF",
-        //                        SportId = sport != null ? sport.Id : sport.Id
-        //                    });
+                        var disciplineName = elements[1].InnerText.Trim();
+                        var disciplineAbbreviation = elements[0].InnerText.Trim();
+                        var disciplines = new List<Discipline>();
+                        if (sport.Name == "Wrestling")
+                        {
+                            disciplines.Add(new Discipline
+                            {
+                                Name = "Wrestling Freestyle",
+                                Code = "WRF",
+                                SportId = sport.Id
+                            });
 
-        //                    disciplines.Add(new Discipline
-        //                    {
-        //                        Name = "Wrestling Greco-Roman",
-        //                        Code = "WRG",
-        //                        SportId = sport != null ? sport.Id : sport.Id
-        //                    });
-        //                }
-        //                else
-        //                {
-        //                    disciplines.Add(new Discipline
-        //                    {
-        //                        Name = disciplineName,
-        //                        Code = disciplineAbbreviation,
-        //                        SportId = sport != null ? sport.Id : sport.Id
-        //                    });
-        //                }
+                            disciplines.Add(new Discipline
+                            {
+                                Name = "Wrestling Greco-Roman",
+                                Code = "WRG",
+                                SportId = sport.Id
+                            });
+                        }
+                        else
+                        {
+                            disciplines.Add(new Discipline
+                            {
+                                Name = disciplineName,
+                                Code = disciplineAbbreviation,
+                                SportId = sport.Id
+                            });
+                        }
 
-        //                //foreach (var discipline in disciplines.Where(x => x.Name != "Canoe Marathon"))
-        //                //{
-        //                //    await this.disciplinesService.AddOrUpdateAsync(discipline);
-        //                //}
-        //            }
-        //        }
-        //    }
-        //}
-        //catch (Exception ex)
-        //{
-        //    this.Logger.LogError(ex, $"Failed to process group: {group.Identifier}");
-        //}
+                        foreach (var discipline in disciplines.Where(x => x.Name != "Canoe Marathon"))
+                        {
+                            var dbDiscipline = await this.disciplineRepository.GetAsync(x => x.Name == discipline.Name);
+                            if (dbDiscipline == null)
+                            {
+                                await this.disciplineRepository.AddAsync(discipline);
+                                await this.disciplineRepository.SaveChangesAsync();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            this.Logger.LogError(ex, $"Failed to process group: {group.Identifier}");
+        }
     }
 }
