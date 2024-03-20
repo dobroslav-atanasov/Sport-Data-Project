a1a7b5497afb3237c64b56b153e8afb53cf8afc0
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-03-20 09:02:12 +0200

    Refactoring.

--- SportData.API/src/Applications/SportData.Converters/OlympicGames/AthleteConverter.cs
+++ SportData.API/src/Applications/SportData.Converters/OlympicGames/AthleteConverter.cs
@@ -4,92 +4,273 @@ using System.Threading.Tasks;
 
 using Microsoft.Extensions.Logging;
 
+using SportData.Common.Constants;
 using SportData.Converters;
 using SportData.Data.Models.Entities.Crawlers;
+using SportData.Data.Models.Entities.OlympicGames;
+using SportData.Data.Models.Entities.OlympicGames.Enumerations;
 using SportData.Services.Data.CrawlerStorageDb.Interfaces;
+using SportData.Services.Data.OlympicGamesDb;
 using SportData.Services.Data.OlympicGamesDb.Interfaces;
 using SportData.Services.Interfaces;
 
 public class AthleteConverter : BaseOlympediaConverter
 {
     private readonly IDateService dateService;
+    private readonly OlympicGamesRepositoryService<Athlete> athletesService;
+    private readonly OlympicGamesRepositoryService<Club> clubsService;
+    private int count = 0;
 
     public AthleteConverter(ILogger<BaseConverter> logger, ICrawlersService crawlersService, ILogsService logsService, IGroupsService groupsService, IZipService zipService,
         IRegExpService regExpService, INormalizeService normalizeService, IOlympediaService olympediaService, IDataCacheService dataCacheService,
-        IDateService dateService)
+        IDateService dateService, OlympicGamesRepositoryService<Athlete> athletesService, OlympicGamesRepositoryService<Club> clubsService)
         : base(logger, crawlersService, logsService, groupsService, zipService, regExpService, normalizeService, olympediaService, dataCacheService)
     {
         this.dateService = dateService;
+        this.athletesService = athletesService;
+        this.clubsService = clubsService;
     }
 
     protected override async Task ProcessGroupAsync(Group group)
     {
-        //try
-        //{
-        //    var document = this.CreateHtmlDocument(group.Documents.Single());
-        //    var number = int.Parse(new Uri(group.Documents.Single().Url).Segments.Last());
-        //    var name = document.DocumentNode.SelectSingleNode("//h1").InnerText.Trim();
-        //    var typeMatch = this.RegExpService.Match(document.ParsedText, @"<tr>\s*<th>Roles<\/th>\s*<td>(.*?)<\/td><\/tr>");
-        //    var genderMatch = this.RegExpService.Match(document.ParsedText, @"<tr>\s*<th>Sex<\/th>\s*<td>(Male|Female)<\/td><\/tr>");
-        //    var bornMatch = this.RegExpService.Match(document.ParsedText, @"<tr>\s*<th>Born<\/th>\s*<td>(.*?)<\/td><\/tr>");
-        //    var diedMatch = this.RegExpService.Match(document.ParsedText, @"<tr>\s*<th>Died<\/th>\s*<td>(.*?)<\/td><\/tr>");
+        try
+        {
+            await Console.Out.WriteLineAsync($"{this.count++}");
+            var document = this.CreateHtmlDocument(group.Documents.Single());
+            var code = int.Parse(new Uri(group.Documents.Single().Url).Segments.Last());
+            var name = document.DocumentNode.SelectSingleNode("//h1").InnerText.Trim();
+            var typeMatch = this.RegExpService.Match(document.ParsedText, @"<tr>\s*<th>Roles<\/th>\s*<td>(.*?)<\/td><\/tr>");
+            var genderMatch = this.RegExpService.Match(document.ParsedText, @"<tr>\s*<th>Sex<\/th>\s*<td>(Male|Female)<\/td><\/tr>");
+            var gender = this.MapGenderEnum(genderMatch.Groups[1].Value);
+            var bornMatch = this.RegExpService.Match(document.ParsedText, @"<tr>\s*<th>Born<\/th>\s*<td>(.*?)<\/td><\/tr>");
+            var diedMatch = this.RegExpService.Match(document.ParsedText, @"<tr>\s*<th>Died<\/th>\s*<td>(.*?)<\/td><\/tr>");
+            var roles = this.GetAthleteRoles(typeMatch.Groups[1].Value);
+            var bornPlace = this.ExtractCityAndCountry(bornMatch);
+            var diedPlace = this.ExtractCityAndCountry(diedMatch);
+            var clubs = await this.GetClubsAsync(document.ParsedText);
 
-        //    var athlete = new Athlete
-        //    {
-        //        Name = name,
-        //        Number = number,
-        //        EnglishName = this.NormalizeService.ReplaceNonEnglishLetters(name),
-        //        Type = this.NormalizeService.MapAthleteType(typeMatch.Groups[1].Value),
-        //        Gender = genderMatch.Groups[1].Value.UpperFirstChar().ToEnum<GenderType>(),
-        //        FullName = this.RegExpService.MatchFirstGroup(document.ParsedText, @"<tr>\s*<th>Full name<\/th>\s*<td>(.*?)<\/td><\/tr>")?.Replace("•", " "),
-        //        Association = this.ExtractAssociations(document.ParsedText),
-        //        Description = this.RegExpService.CutHtml(this.RegExpService.MatchFirstGroup(document.ParsedText, @"<div class=(?:""|')description(?:""|')>(.*?)<\/div>")),
-        //        Nationality = this.ExtractNationality(document.ParsedText),
-        //        BirthDate = bornMatch != null ? this.dateService.ParseDate(bornMatch.Groups[1].Value).From : null,
-        //        BirthPlace = bornMatch != null ? this.RegExpService.MatchFirstGroup(bornMatch.Groups[1].Value, @"<a href=(?:""|')\/place_names\/[\d]+(?:""|')>(.*?)<\/a>") : null,
-        //        DiedDate = diedMatch != null ? this.dateService.ParseDate(diedMatch.Groups[1].Value).From : null,
-        //        DiedPlace = diedMatch != null ? this.RegExpService.MatchFirstGroup(diedMatch.Groups[1].Value, @"<a href=(?:""|')\/place_names\/[\d]+(?:""|')>(.*?)<\/a>") : null,
-        //    };
+            var athlete = new Athlete
+            {
+                Code = code,
+                GenderId = (int)gender,
+                Name = name,
 
-        //    var measurmentsMatch = this.RegExpService.Match(document.ParsedText, @"<tr>\s*<th>Measurements<\/th>\s*<td>(.*?)<\/td><\/tr>");
-        //    if (measurmentsMatch != null)
-        //    {
-        //        var heightMatch = this.RegExpService.Match(measurmentsMatch.Groups[1].Value, @"([\d]+)\s*cm");
-        //        if (heightMatch != null)
-        //        {
-        //            athlete.Height = int.Parse(heightMatch.Groups[1].Value);
-        //        }
+                TranslateName = this.NormalizeService.ReplaceNonEnglishLetters(name),
+                FullName = this.RegExpService.MatchFirstGroup(document.ParsedText, @"<tr>\s*<th>Full name<\/th>\s*<td>(.*?)<\/td><\/tr>")?.Replace("•", " "),
+                OriginalName = this.RegExpService.MatchFirstGroup(document.ParsedText, @"<tr>\s*<th>Original name<\/th>\s*<td>(.*?)<\/td><\/tr>")?.Replace("•", " "),
+                Citizenship = this.ExtractCitizenship(document.ParsedText),
+                BirthDate = bornMatch != null ? this.dateService.ParseDate(bornMatch.Groups[1].Value).From : null,
+                BirthCity = bornPlace.Item1,
+                BirthCountry = bornPlace.Item2,
+                DiedDate = diedMatch != null ? this.dateService.ParseDate(diedMatch.Groups[1].Value).From : null,
+                DiedCity = diedPlace.Item1,
+                DiedCountry = diedPlace.Item2,
+                Description = this.RegExpService.CutHtml(this.RegExpService.MatchFirstGroup(document.ParsedText, @"<div class=(?:""|')description(?:""|')>(.*?)<\/div>")),
+                Roles = roles.Select(x => new Role { AthleteTypeId = (int)x }).ToList(),
+                AthletesClubs = clubs.Select(x => new AthleteClub { ClubId = x.Id }).ToList(),
+            };
 
-        //        var weightMatch = this.RegExpService.Match(measurmentsMatch.Groups[1].Value, @"([\d]+)\s*kg");
-        //        if (weightMatch != null)
-        //        {
-        //            athlete.Weight = int.Parse(weightMatch.Groups[1].Value);
-        //        }
-        //    }
+            var measurmentsMatch = this.RegExpService.Match(document.ParsedText, @"<tr>\s*<th>Measurements<\/th>\s*<td>(.*?)<\/td><\/tr>");
+            if (measurmentsMatch != null)
+            {
+                var heightMatch = this.RegExpService.Match(measurmentsMatch.Groups[1].Value, @"([\d]+)\s*cm");
+                if (heightMatch != null)
+                {
+                    athlete.HeightInCentimeters = int.Parse(heightMatch.Groups[1].Value);
+                    athlete.HeightInInches = athlete.HeightInCentimeters / GlobalConstants.INCHES;
+                }
 
-        //    //athlete = await athletesService.AddOrUpdateAsync(athlete);
+                var weightMatch = this.RegExpService.Match(measurmentsMatch.Groups[1].Value, @"([\d]+)\s*kg");
+                if (weightMatch != null)
+                {
+                    athlete.WeightInKilograms = int.Parse(weightMatch.Groups[1].Value);
+                    athlete.WeightInPounds = (int)(athlete.WeightInKilograms * GlobalConstants.POUNDS);
+                }
+            }
 
-        //    //var nocMatch = this.RegExpService.Match(document.ParsedText, @"<tr>\s*<th>NOC(?:\(s\))?<\/th>\s*<td>(.*?)<\/td>\s*<\/tr>");
-        //    //if (nocMatch != null)
-        //    //{
-        //    //    var countryCodes = this.OlympediaService.FindNOCCodes(nocMatch.Groups[1].Value);
-        //    //    foreach (var code in countryCodes)
-        //    //    {
-        //    //        var nocCacheModel = this.DataCacheService.NOCCacheModels.FirstOrDefault(c => c.Code == code);
-        //    //        if (nocCacheModel != null)
-        //    //        {
-        //    //            await this.nationalitiesService.AddOrUpdateAsync(new Nationality { AthleteId = athlete.Id, NOCId = nocCacheModel.Id });
-        //    //        }
-        //    //    }
-        //    //}
-        //}
-        //catch (Exception ex)
-        //{
-        //    this.Logger.LogError(ex, $"Failed to process group: {group.Identifier}");
-        //}
+            var dbAthlete = await this.athletesService.GetAsync(x => x.Code == code);
+            if (dbAthlete != null)
+            {
+                var equals = athlete.Equals(dbAthlete);
+                if (!equals)
+                {
+                    this.athletesService.Update(dbAthlete);
+                }
+            }
+            else
+            {
+                dbAthlete = await this.athletesService.AddAsync(athlete);
+            }
+
+            //var dbAthlete = await this.athleteRepository.GetAsync(x => x.Code == code);
+            //if (dbAthlete != null)
+            //{
+            //    var equals = athlete.Equals(dbAthlete);
+            //    if (!equals)
+            //    {
+            //        this.athleteRepository.Update(dbAthlete);
+            //        await this.athleteRepository.SaveChangesAsync();
+            //    }
+            //}
+            //else
+            //{
+            //    await this.athleteRepository.AddAsync(athlete);
+            //    await this.athleteRepository.SaveChangesAsync();
+            //}
+
+            //foreach (var athleteTypeCache in roles)
+            //{
+            //    var dbRole = await this.roleRepository.GetAsync(x => x.AthleteId == dbAthlete.Id && x.AthleteTypeId == athleteTypeCache.Id);
+            //    if (dbRole == null)
+            //    {
+            //        await this.roleRepository.AddAsync(new Role { AthleteId = dbAthlete.Id, AthleteTypeId = athleteTypeCache.Id });
+            //        await this.roleRepository.SaveChangesAsync();
+            //    }
+            //}
+
+            //foreach (var club in clubs)
+            //{
+            //    var dbAthleteClub = await this.athleteClubRepository.GetAsync(x => x.AthleteId == dbAthlete.Id && x.ClubId == club.Id);
+            //    if (dbAthleteClub == null)
+            //    {
+            //        await this.athleteClubRepository.AddAsync(new AthleteClub { AthleteId = dbAthlete.Id, ClubId = club.Id });
+            //        await this.athleteClubRepository.SaveChangesAsync();
+            //    }
+            //}
+        }
+        catch (Exception ex)
+        {
+            this.Logger.LogError(ex, $"Failed to process group: {group.Identifier}");
+        }
     }
 
-    private string ExtractNationality(string text)
+    public Tuple<string, string> ExtractCityAndCountry(System.Text.RegularExpressions.Match match)
+    {
+        if (match != null)
+        {
+            var newMatch = this.RegExpService.Match(match.Groups[1].Value, @">(.*?)?(\([A-Z]{3}\))?<");
+            if (newMatch != null)
+            {
+                return new Tuple<string, string>(newMatch.Groups[1].Value.Trim(), newMatch.Groups[2].Value.Trim().Replace("(", string.Empty).Replace(")", string.Empty));
+            }
+
+        }
+
+        return new Tuple<string, string>(null, null);
+    }
+
+    public List<AthleteTypeEnum> GetAthleteRoles(string text)
+    {
+        text = text?.Replace(" •", ",");
+        var athleteTypes = new List<AthleteTypeEnum>();
+        switch (text)
+        {
+            case "Competed in Intercalated Games":
+            case "Competed in Intercalated Games, Non-starter":
+            case "Competed in Olympic Games":
+            case "Competed in Olympic Games (non-medal events)":
+            case "Competed in Olympic Games (non-medal events), Competed in Intercalated Games":
+            case "Competed in Olympic Games (non-medal events), Non-starter":
+            case "Competed in Olympic Games, Competed in Intercalated Games":
+            case "Competed in Olympic Games, Competed in Intercalated Games, Non-starter":
+            case "Competed in Olympic Games, Competed in Olympic Games (non-medal events)":
+            case "Competed in Olympic Games, Competed in Olympic Games (non-medal events), Competed in Intercalated Games":
+            case "Competed in Olympic Games, Competed in Olympic Games (non-medal events), Non-starter":
+            case "Competed in Olympic Games, Competed in Youth Olympic Games":
+            case "Competed in Olympic Games, Competed in Youth Olympic Games (non-medal events)":
+            case "Competed in Olympic Games, Competed in Youth Olympic Games, Non-starter":
+            case "Competed in Olympic Games, Non-starter":
+            case "Competed in Olympic Games, Other":
+            case "Competed in Youth Olympic Games":
+            case "Competed in Youth Olympic Games, Non-starter":
+            case "Non-starter":
+            case "Non-starter, Other":
+            case "Other":
+                athleteTypes.Add(AthleteTypeEnum.Athlete);
+                break;
+            case "Coach":
+                athleteTypes.Add(AthleteTypeEnum.Coach);
+                break;
+            case "Referee":
+                athleteTypes.Add(AthleteTypeEnum.Referee);
+                break;
+            case "Coach, Other":
+            case "Competed in Olympic Games (non-medal events), Coach":
+            case "Competed in Olympic Games, Coach":
+            case "Competed in Olympic Games, Competed in Olympic Games (non-medal events), Coach":
+            case "Competed in Olympic Games, Non-starter, Coach":
+            case "Non-starter, Coach":
+                athleteTypes.Add(AthleteTypeEnum.Athlete);
+                athleteTypes.Add(AthleteTypeEnum.Coach);
+                break;
+            case "Coach, Referee":
+                athleteTypes.Add(AthleteTypeEnum.Coach);
+                athleteTypes.Add(AthleteTypeEnum.Referee);
+                break;
+            case "Competed in Olympic Games, Competed in Intercalated Games, Non-starter, Referee":
+            case "Competed in Olympic Games, Competed in Intercalated Games, Referee":
+            case "Competed in Olympic Games, Competed in Olympic Games (non-medal events), Competed in Intercalated Games, Referee":
+            case "Competed in Olympic Games, Competed in Olympic Games (non-medal events), Referee":
+            case "Competed in Olympic Games, Non-starter, Referee":
+            case "Competed in Olympic Games, Referee":
+            case "Non-starter, Referee":
+            case "Referee, Other":
+                athleteTypes.Add(AthleteTypeEnum.Athlete);
+                athleteTypes.Add(AthleteTypeEnum.Referee);
+                break;
+            case "Competed in Olympic Games, Coach, Referee":
+            case "Competed in Olympic Games, Non-starter, Coach, Referee":
+            case "Non-starter, Coach, Referee":
+                athleteTypes.Add(AthleteTypeEnum.Athlete);
+                athleteTypes.Add(AthleteTypeEnum.Coach);
+                athleteTypes.Add(AthleteTypeEnum.Referee);
+                break;
+            case "Competed in Olympic Games, IOC member":
+            case "Competed in Olympic Games, Non-starter, IOC member":
+                athleteTypes.Add(AthleteTypeEnum.Athlete);
+                athleteTypes.Add(AthleteTypeEnum.IOCMember);
+                break;
+            case "Competed in Olympic Games, IOC member, Referee":
+                athleteTypes.Add(AthleteTypeEnum.Athlete);
+                athleteTypes.Add(AthleteTypeEnum.Referee);
+                athleteTypes.Add(AthleteTypeEnum.IOCMember);
+                break;
+            case "IOC member":
+                athleteTypes.Add(AthleteTypeEnum.IOCMember);
+                break;
+            case "IOC member, Coach":
+                athleteTypes.Add(AthleteTypeEnum.Coach);
+                athleteTypes.Add(AthleteTypeEnum.IOCMember);
+                break;
+            case "IOC member, Referee":
+                athleteTypes.Add(AthleteTypeEnum.Referee);
+                athleteTypes.Add(AthleteTypeEnum.IOCMember);
+                break;
+            default:
+                athleteTypes.Add(AthleteTypeEnum.None);
+                break;
+        }
+
+        return athleteTypes;
+    }
+
+    private GenderTypeEnum MapGenderEnum(string text)
+    {
+        var gender = GenderTypeEnum.None;
+
+        if (text.StartsWith("male", StringComparison.CurrentCultureIgnoreCase))
+        {
+            gender = GenderTypeEnum.Male;
+        }
+        else if (text.StartsWith("female", StringComparison.CurrentCultureIgnoreCase))
+        {
+            gender = GenderTypeEnum.Female;
+        }
+
+        return gender;
+    }
+
+    private string ExtractCitizenship(string text)
     {
         var match = this.RegExpService.Match(text, @"<tr>\s*<th>Nationality<\/th>\s*<td>(.*?)<\/td><\/tr>");
         if (match != null)
@@ -104,23 +285,29 @@ public class AthleteConverter : BaseOlympediaConverter
         return null;
     }
 
-    private string ExtractAssociations(string text)
+    private async Task<IEnumerable<Club>> GetClubsAsync(string text)
     {
         var associationMatch = this.RegExpService.Match(text, @"<tr>\s*<th>Affiliations<\/th>\s*<td>(.*?)<\/td><\/tr>");
+        var clubs = new List<Club>();
         if (associationMatch != null)
         {
-            var matches = this.RegExpService.Matches(associationMatch.Groups[1].Value, @"<a href=""/affiliations/(\d+)"">(.*?)</a>");
-            if (matches != null && matches.Count != 0)
+            var matches = this.RegExpService.Matches(associationMatch.Groups[1].Value, @">(.*?)?(?:\s*,\s*)(.*?)?(\([A-Z]{3}\))?<");
+            foreach (System.Text.RegularExpressions.Match match in matches)
             {
-                var result = string.Join("|", matches.Select(x => x.Groups[2].Value.Trim()).ToList());
-                return result;
-            }
-            else
-            {
-                return associationMatch.Groups[1].Value.Trim();
+                var club = new Club
+                {
+                    Name = match.Groups[1].Value,
+                    City = match.Groups[2].Value,
+                    Country = match.Groups[3].Value.Replace("(", string.Empty).Replace(")", string.Empty),
+                };
+
+                var dbClub = await this.clubsService.GetAsync(x => x.Name == club.Name && x.City == club.City && x.Country == club.Country);
+                dbClub ??= await this.clubsService.AddAsync(club);
+
+                clubs.Add(dbClub);
             }
         }
 
-        return null;
+        return clubs;
     }
 }
