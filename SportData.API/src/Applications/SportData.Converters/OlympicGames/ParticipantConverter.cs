5670de9ca94dd892d9db7163c49bec00ff380e96
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-03-27 21:36:29 +0200

    The converter was modified.

--- SportData.API/src/Applications/SportData.Converters/OlympicGames/ParticipantConverter.cs
+++ SportData.API/src/Applications/SportData.Converters/OlympicGames/ParticipantConverter.cs
@@ -4,16 +4,33 @@ using System.Threading.Tasks;
 
 using Microsoft.Extensions.Logging;
 
+using SportData.Common.Constants;
+using SportData.Data.Models.Cache;
 using SportData.Data.Models.Entities.Crawlers;
+using SportData.Data.Models.Entities.OlympicGames;
+using SportData.Data.Models.Entities.OlympicGames.Enumerations;
 using SportData.Services.Data.CrawlerStorageDb.Interfaces;
+using SportData.Services.Data.OlympicGamesDb;
+using SportData.Services.Data.OlympicGamesDb.Interfaces;
 using SportData.Services.Interfaces;
 
 public class ParticipantConverter : BaseOlympediaConverter
 {
+    private readonly OlympicGamesRepositoryService<Participation> participationsService;
+    private readonly OlympicGamesRepositoryService<Athlete> athletesService;
+    private readonly OlympicGamesRepositoryService<Team> teamsService;
+    private readonly OlympicGamesRepositoryService<Squad> squadsService;
+
     public ParticipantConverter(ILogger<BaseConverter> logger, ICrawlersService crawlersService, ILogsService logsService, IGroupsService groupsService, IZipService zipService,
-        IRegExpService regExpService, INormalizeService normalizeService, IOlympediaService olympediaService)
-        : base(logger, crawlersService, logsService, groupsService, zipService, regExpService, normalizeService, olympediaService)
+        IRegExpService regExpService, INormalizeService normalizeService, IOlympediaService olympediaService, IDataCacheService dataCacheService,
+        OlympicGamesRepositoryService<Participation> participationsService, OlympicGamesRepositoryService<Athlete> athletesService, OlympicGamesRepositoryService<Team> teamsService,
+        OlympicGamesRepositoryService<Squad> squadsService)
+        : base(logger, crawlersService, logsService, groupsService, zipService, regExpService, normalizeService, olympediaService, dataCacheService)
     {
+        this.participationsService = participationsService;
+        this.athletesService = athletesService;
+        this.teamsService = teamsService;
+        this.squadsService = squadsService;
     }
 
     protected override async Task ProcessGroupAsync(Group group)
@@ -22,82 +39,102 @@ public class ParticipantConverter : BaseOlympediaConverter
         {
             var document = this.CreateHtmlDocument(group.Documents.Single(x => x.Order == 1));
             var originalEventName = document.DocumentNode.SelectSingleNode("//ol[@class='breadcrumb']/li[@class='active']").InnerText;
-            var gameCacheModel = this.FindGame(document);
-            var discipline = this.FindDiscipline(document);
-            var eventModel = this.CreateEventModel(originalEventName, gameCacheModel, discipline);
+            var gameCache = this.GetGameFromDatabase(document);
+            var disciplineCache = this.GetDisciplineFromDatabase(document);
+            var eventModel = this.CreateEventModel(originalEventName, gameCache, disciplineCache);
             if (eventModel != null)
             {
-                //var eventCacheModel = this.DataCacheService
-                //    .EventCacheModels
-                //    .FirstOrDefault(x => x.OriginalName == eventModel.OriginalName && x.GameId == eventModel.GameId && x.DisciplineId == eventModel.DisciplineId);
+                var eventCache = this.DataCacheService.Events
+                    .FirstOrDefault(x => x.OriginalName == eventModel.OriginalName && x.GameId == eventModel.GameId && x.DisciplineId == eventModel.DisciplineId);
+                if (eventCache != null)
+                {
+                    var trRows = document.DocumentNode.SelectSingleNode("//table[@class='table table-striped']").Elements("tr");
+                    var indexes = this.GetHeaderIndexes(document);
+                    if (eventCache.IsTeamEvent)
+                    {
+                        Team team = null;
+                        foreach (var trRow in trRows)
+                        {
+                            var tdNodes = trRow.Elements("td").ToList();
+                            var countryCode = this.OlympediaService.FindNOCCode(trRow.OuterHtml);
+                            if (countryCode != null && !trRow.InnerHtml.Contains("coach", StringComparison.CurrentCultureIgnoreCase))
+                            {
+                                var nocCache = this.DataCacheService.NOCs.FirstOrDefault(x => x.Code == countryCode);
+                                var medalTypeEnum = this.OlympediaService.FindMedal(trRow.OuterHtml);
+                                var finishTypeEnum = this.OlympediaService.FindStatus(trRow.OuterHtml);
+                                team = new Team
+                                {
+                                    NOCId = nocCache.Id,
+                                    EventId = eventCache.Id,
+                                    Name = tdNodes[indexes[ConverterConstants.INDEX_NAME]].InnerText.Trim(),
+                                    MedalId = (int)medalTypeEnum,
+                                    FinishTypeId = (int)finishTypeEnum
+                                };
 
-                //if (eventCacheModel != null)
-                //{
-                //    var trRows = document.DocumentNode.SelectSingleNode("//table[@class='table table-striped']").Elements("tr");
-                //    var indexes = this.GetHeaderIndexes(document);
-                //    if (eventCacheModel.IsTeamEvent)
-                //    {
-                //        Team team = null;
-                //        foreach (var trRow in trRows)
-                //        {
-                //            var tdNodes = trRow.Elements("td").ToList();
-                //            var countryCode = this.OlympediaService.FindNOCCode(trRow.OuterHtml);
-                //            if (countryCode != null && !trRow.InnerHtml.ToLower().Contains("coach"))
-                //            {
-                //                var nocCacheModel = this.DataCacheService.NOCCacheModels.FirstOrDefault(x => x.Code == countryCode);
-                //                team = new Team
-                //                {
-                //                    NOCId = nocCacheModel.Id,
-                //                    EventId = eventCacheModel.Id,
-                //                    Name = tdNodes[indexes[ConverterConstants.INDEX_NAME]].InnerText.Trim(),
-                //                    Medal = this.OlympediaService.FindMedal(trRow.OuterHtml),
-                //                    FinishStatus = this.OlympediaService.FindStatus(trRow.OuterHtml)
-                //                };
+                                var dbTeam = await this.teamsService.GetAsync(x => x.Name == team.Name && x.EventId == team.EventId && x.NOCId == team.NOCId);
+                                if (dbTeam != null)
+                                {
+                                    var equals = team.Equals(dbTeam);
+                                    if (!equals)
+                                    {
+                                        this.teamsService.Update(dbTeam);
+                                    }
 
-                //                team = await this.teamsService.AddOrUpdateAsync(team);
-                //            }
+                                    team = dbTeam;
+                                }
+                                else
+                                {
+                                    await this.teamsService.AddAsync(team);
+                                }
+                            }
 
-                //            if (trRow.InnerHtml.ToLower().Contains("coach"))
-                //            {
-                //                var athleteModel = this.OlympediaService.FindAthlete(trRow.OuterHtml);
-                //                var coach = await this.athletesService.GetAsync(athleteModel.Code);
-                //                if (coach != null)
-                //                {
-                //                    team.CoachId = coach.Id;
-                //                    team = await this.teamsService.AddOrUpdateAsync(team);
-                //                }
-                //            }
-                //            else
-                //            {
-                //                var athleteNumbers = this.OlympediaService.FindAthletes(trRow.OuterHtml);
-                //                var nocCode = this.DataCacheService.NOCCacheModels.FirstOrDefault(x => x.Id == team.NOCId);
-                //                foreach (var athleteModel in athleteNumbers)
-                //                {
-                //                    var participant = await this.CreateParticipantAsync(athleteModel.Code, nocCode.Code, team.Medal, team.FinishStatus, eventCacheModel, gameCacheModel);
-
-                //                    if (participant != null)
-                //                    {
-                //                        await this.squadsService.AddOrUpdateAsync(new Squad { ParticipantId = participant.Id, TeamId = team.Id });
-                //                    }
-                //                }
-                //            }
-                //        }
-                //    }
-                //    else
-                //    {
-                //        foreach (var trRow in trRows)
-                //        {
-                //            var athleteModel = this.OlympediaService.FindAthlete(trRow.OuterHtml);
-                //            var countryCode = this.OlympediaService.FindNOCCode(trRow.OuterHtml);
-                //            var medalType = this.OlympediaService.FindMedal(trRow.OuterHtml);
-                //            var finishStatus = this.OlympediaService.FindStatus(trRow.OuterHtml);
-                //            if (athleteModel != null && countryCode != null)
-                //            {
-                //                await this.CreateParticipantAsync(athleteModel.Code, countryCode, medalType, finishStatus, eventCacheModel, gameCacheModel);
-                //            }
-                //        }
-                //    }
-                //}
+                            if (trRow.InnerHtml.ToLower().Contains("coach"))
+                            {
+                                var athleteModel = this.OlympediaService.FindAthlete(trRow.OuterHtml);
+                                if (athleteModel != null)
+                                {
+                                    var coach = await this.athletesService.GetAsync(x => x.Code == athleteModel.Code);
+                                    if (coach != null)
+                                    {
+                                        team.CoachId = coach.Id;
+                                        team = this.teamsService.Update(team);
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                var athleteNumbers = this.OlympediaService.FindAthletes(trRow.OuterHtml);
+                                var nocCode = this.DataCacheService.NOCs.FirstOrDefault(x => x.Id == team.NOCId);
+                                foreach (var athleteModel in athleteNumbers)
+                                {
+                                    var participant = await this.CreateParticipantAsync(athleteModel.Code, nocCode.Code, (MedalTypeEnum)team.MedalId, (FinishTypeEnum)team.FinishTypeId, eventCache, gameCache);
+                                    if (participant != null)
+                                    {
+                                        var dbSquad = await this.squadsService.GetAsync(x => x.ParticipationId == participant.Id && x.TeamId == team.Id);
+                                        if (dbSquad == null)
+                                        {
+                                            await this.squadsService.AddAsync(new Squad { ParticipationId = participant.Id, TeamId = team.Id });
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        foreach (var trRow in trRows)
+                        {
+                            var athleteModel = this.OlympediaService.FindAthlete(trRow.OuterHtml);
+                            var countryCode = this.OlympediaService.FindNOCCode(trRow.OuterHtml);
+                            var medalType = this.OlympediaService.FindMedal(trRow.OuterHtml);
+                            var finishStatus = this.OlympediaService.FindStatus(trRow.OuterHtml);
+                            if (athleteModel != null && countryCode != null)
+                            {
+                                await this.CreateParticipantAsync(athleteModel.Code, countryCode, medalType, finishStatus, eventCache, gameCache);
+                            }
+                        }
+                    }
+                }
             }
         }
         catch (Exception ex)
@@ -106,46 +143,66 @@ public class ParticipantConverter : BaseOlympediaConverter
         }
     }
 
-    //private async Task<Participant> CreateParticipantAsync(int athleteNumber, string countryCode, MedalType medalType, FinishStatus finishStatus, EventCacheModel eventCacheModel, GameCacheModel gameCacheModel)
-    //{
-    //    //var athlete = await this.athletesService.GetAsync(athleteNumber);
-    //    //if (athlete != null)
-    //    //{
-    //    //    var nocCacheModel = this.DataCacheService.NOCCacheModels.FirstOrDefault(c => c.Code == countryCode);
-    //    //    if (nocCacheModel != null)
-    //    //    {
-    //    //        var participant = new Participant
-    //    //        {
-    //    //            AthleteId = athlete.Id,
-    //    //            EventId = eventCacheModel.Id,
-    //    //            NOCId = nocCacheModel.Id,
-    //    //            Number = athleteNumber,
-    //    //            Medal = medalType,
-    //    //            FinishStatus = finishStatus,
-    //    //        };
+    private async Task<Participation> CreateParticipantAsync(int athleteCode, string countryCode, MedalTypeEnum medalTypeEnum, FinishTypeEnum finishTypeEnum, EventCache eventCache, GameCache gameCache)
+    {
+        var athlete = await this.athletesService.GetAsync(x => x.Code == athleteCode);
+        if (athlete != null)
+        {
+            var nocCacheModel = this.DataCacheService.NOCs.FirstOrDefault(c => c.Code == countryCode);
+            if (nocCacheModel != null)
+            {
+                var participation = new Participation
+                {
+                    AthleteId = athlete.Id,
+                    EventId = eventCache.Id,
+                    NOCId = nocCacheModel.Id,
+                    Code = athleteCode,
+                    MedalId = (int)medalTypeEnum,
+                    FinishTypeId = (int)finishTypeEnum,
+                };
 
-    //    //        if (athlete.BirthDate.HasValue)
-    //    //        {
-    //    //            this.CalculateAge(gameCacheModel.OpenDate ?? gameCacheModel.StartCompetitionDate.Value, athlete.BirthDate.Value, participant);
-    //    //        }
+                if (athlete.BirthDate.HasValue)
+                {
+                    var age = this.CalculateAge(gameCache.OpeningDate ?? gameCache.StartCompetitionDate.Value, athlete.BirthDate.Value);
+                    participation.AgeYears = age.Item1;
+                    participation.AgeDays = age.Item2;
+                }
 
-    //    //        participant = await this.participantsService.AddOrUpdateAsync(participant);
+                var dbParticipation = await this.participationsService.GetAsync(x => x.AthleteId == participation.AthleteId && x.EventId == participation.EventId && x.NOCId == participation.NOCId);
+                if (dbParticipation != null)
+                {
+                    var equals = participation.Equals(dbParticipation);
+                    if (!equals)
+                    {
+                        this.participationsService.Update(dbParticipation);
+                    }
+                }
+                else
+                {
+                    dbParticipation = await this.participationsService.AddAsync(participation);
+                }
 
-    //    //        return participant;
-    //    //    }
-    //    //}
+                return dbParticipation;
+            }
+        }
 
-    //    return null;
-    //}
+        return null;
+    }
 
-    //private void CalculateAge(DateTime startDate, DateTime endDate, Participant participant)
-    //{
-    //    var totalDays = (startDate - endDate).TotalDays;
-    //    var year = (int)Math.Floor(totalDays / 365.25);
-    //    var newYear = endDate.AddYears(year);
-    //    var days = (startDate - newYear).TotalDays;
+    private Tuple<int?, int?> CalculateAge(DateTime? startDate, DateTime? endDate)
+    {
+        var age = new Tuple<int?, int?>(null, null);
+        if (!startDate.HasValue || !endDate.HasValue)
+        {
+            return age;
+        }
 
-    //    participant.AgeYears = year;
-    //    participant.AgeDays = (int)days;
-    //}
+        var totalDays = (startDate.Value - endDate.Value).TotalDays;
+        var year = (int)Math.Floor(totalDays / 365.25);
+        var newYear = endDate.Value.AddYears(year);
+        var days = (startDate.Value - newYear).TotalDays;
+        age = new Tuple<int?, int?>(year, (int)days);
+
+        return age;
+    }
 }
